1eef36d8d013cd2d5db6cd2318c4ea27ff236ac4
nodeexcel <vaibhav.excel2011@gmail.com>
2018-04-13 11:25:29 +0530

    updated

--- api/payment.js
+++ api/payment.js
@@ -9,13 +9,13 @@ router.post('', function(req, res) {
 router.post('/stripe_payment', function(req, res) {
     var token = req.body.stripeToken;
     var charge = stripe.charges.create({
-        amount: req.body.amount,
+        amount: req.body.amount * 100,
         currency: req.body.currency,
         description: req.body.description,
         source: token,
     }, function(err, charge) {
         if (err) {
-            res.status(401).json({ error: 1, message: "error occured", err: err })
+            res.status(400).json({ error: 1, message: "error occured", err: err })
         } else {
             console.log('success payment');
             res.status(200).json({ error: 0, charges: charge })
