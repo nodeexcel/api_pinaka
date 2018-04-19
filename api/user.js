1c671538a8d093f7469d9f7e7f10a33aa09a231c
nodeexcel <vaibhav.excel2011@gmail.com>
2018-04-19 11:14:27 +0530

    US country code

--- api/user.js
+++ api/user.js
@@ -233,7 +233,7 @@ router.post('/signup', function(req, res) {
 });
 
 router.post('/sendcode', function(req, res) {
-    var phone = req.body.phone;
+    var phone = "+1" + req.body.phone;
 
     var regPhone = /^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/;
 
